67ba3d383e8757267011bd2e75c301d4e1c402f0
Johnny Addis <johnnyaddis23@gmail.com>
2024-12-19 21:07:31 -0600

    Adding user input for neccesary variables

---
+++ profits_calculator.py
@@ -0,0 +1,14 @@
+option_type = int(input('What type of Option Order? Long Call, Short Call, Long Put, or Short Put. O for long call, 1 for short call, ...etc'))
+
+
+current_price = input('What is the current price of the underlying asset?')
+current_price = float(current_price)
+
+strike_price = input('What is the strike price?')
+strike_price = float(strike_price)
+
+premium = input('What is the premium per share?')
+premium = float(premium)
+
+
+
